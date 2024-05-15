12b4f0011757f504985e37e9202f76ee2c44563e
JoaoVitor2004 <joaovitorsouzaa28122004@gmail.com>
2024-05-15 16:05:11 -0300

    novas alterações

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-const botaoAbrir = document.querySelector('#abrir')
-const botaoFechar = document.querySelector('#fechar')
 const modal = document.querySelector('#modal')
 const fade = document.querySelector('#fade')
-const input1 = document.querySelector('.int1')
-const input2 = document.querySelector('.int2')
+const inputs = document.querySelectorAll('.input')
+const form = document.getElementById('form')
+const texto = document.getElementById('res')
 
-function limparClasse(){
+function exibirErro(indice){
     modal.classList.toggle('hide')
     fade.classList.toggle('hide')
-    botaoFechar.classList.toggle('hide')
+    inputs[indice].style.borderBottom = '2px solid rgb(250, 67, 67'
 }
 
-botaoAbrir.addEventListener('click', () => {
+function limparErro() {
+    setTimeout(() => {
+        modal.classList.add('hide')
+        fade.classList.add('hide')
+    }, 2200)
+}
 
-    if(input1.value === '' || input2.value === '') {
-        alert('Complete todos os campos')
-    } else {
-        limparClasse()
+function validarEmail(email) {
+    const resultado = new RegExp(
+        /^[a-zA-Z._-]+@[a-zA-Z]+\.[a-zA-Z]/
+    )
+
+    if (resultado.test(email)) {
+        return true
     }
 
-})
+    return false
+}
 
-botaoFechar.addEventListener('click', () => {
-    limparClasse()
+form.addEventListener('submit', (remove) => {
+        
+    remove.preventDefault()
+
+    if (!validarEmail(inputs[0].value)) {
+        exibirErro(0)
+        limparErro()
+        texto.innerHTML = 'Digite um email valido'
+        return
+    }
+
+    if (inputs[1].value.length < 8) {
+        exibirErro(1)
+        limparErro()
+        texto.innerHTML = 'Digite uma senha com no minimo 8 caracteres'
+        return
+    }
+
+    alert('Bem vindo de volta')
+    form.submit()
+
 })
